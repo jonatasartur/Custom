443e1178b3dfdcbce662c712ea56d7d40bf1b9f0
Jonatas Dias <jonatas_artur@hotmail.com>
2020-04-21 17:29:38 -0300

    Remove

--- backend_debranding/static/src/js/announcement.js
+++
@@ -1,9 +0,0 @@
-odoo.define('backend_debranding_v12.announcement', function(require) {
-    "use strict";
-
-    //    require('mail.announcement');
-    var WebClient = require('web.WebClient');
-    WebClient.include({
-        show_annoucement_bar: function() {}
-    });
-});
