5770e9c4f3647ec47c94932753bd92a0c441284b
Jonatas Artur <jonatas_artur@hotmail.com>
2020-06-01 18:36:25 -0300

    Some new apps

---
+++ gtica_shop_description_product_free/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 GTICA C.A. - Ing Henry Vivas
+
+{
+    'name': 'Description Product change in Backend',
+    'summary': 'Make quick changes to your website product description in backend',
+    'version': '12.0.1.0.0',
+    'category': 'Website',
+    'author': 'GTICA C.A',
+    'support': 'controlwebmanager@gmail.com',
+    'license': 'OPL-1',
+    'website': 'https://gtica.online/',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/view_shop_website.xml',
+    ],
+    'images': ['static/description/main_screenshot.png'],
+    'application': False,
+    'installable': True,
+    'license': 'AGPL-3',
+}
