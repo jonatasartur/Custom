c71fa104b79a0a0d96db26108238d6d33dab586a
Jonatas Dias <jonatas_artur@hotmail.com>
2023-09-17 14:10:27 -0300

    Add nem addon
    Add report addon

---
+++ reports_templates/Readme.txt
@@ -0,0 +1,4 @@
+
+=> 12.0.0.3 : Removed Duplicate print report options.
+
+=> 12.0.0.4 : Issue solved regarding tracebacks and taxes in report including the issue of section and note in the report.
---
+++ reports_templates/res_company.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api, _
+
+class res_company(models.Model):
+    _inherit = "res.company"
+
+    sale_template = fields.Selection([
+            ('fency', 'Fency'),
+            ('classic', 'Classic'),
+            ('modern', 'Modern'),
+            ('odoo_standard', 'Odoo Standard'),
+        ], 'Sale')
+    purchase_template = fields.Selection([
+            ('fency', 'Fency'),
+            ('classic', 'Classic'),
+            ('modern', 'Modern'),
+            ('odoo_standard', 'Odoo Standard'),
+        ], 'Purchase')
+    stock_template = fields.Selection([
+            ('fency', 'Fency'),
+            ('classic', 'Classic'),
+            ('modern', 'Modern'),
+            ('odoo_standard', 'Odoo Standard'),
+        ], 'Stock')
+    account_template = fields.Selection([
+            ('fency', 'Fency'),
+            ('classic', 'Classic'),
+            ('modern', 'Modern'),
+            ('odoo_standard', 'Odoo Standard'),
+        ], 'Account')
+
+
+class account_invoice(models.Model):
+    _inherit = "account.invoice"
+
+    paypal_chk = fields.Boolean("Paypal")
+    paypal_id = fields.Char("Paypal Id")
+
+
+
+class res_company(models.Model):
+    _inherit = "res.company"
+
+    bank_account_id = fields.Many2one('res.partner.bank', 'Bank Account')
+
+class res_partner_bank(models.Model):
+    _inherit = "res.partner.bank"
+
+    street = fields.Char('Street')
+    street2 = fields.Char('Street2')
+    zip = fields.Char('Zip', size=24, change_default=True)
+    city = fields.Char('City')
+    state_id = fields.Many2one("res.country.state", 'State')
+    country_id = fields.Many2one('res.country', 'Country')
+    swift_code = fields.Char('Swift Code')
+    ifsc = fields.Char('IFSC')
+    branch_name = fields.Char('Branch Name')
+
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+
